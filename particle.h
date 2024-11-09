7e42f854140a9dec762f4d7308ecc9d6012b6030
jiangjt00 <145301366+jiangjt00@users.noreply.github.com>
2024-11-09 15:03:13 +0800

    Add files via upload

--- particle.h
+++ particle.h
@@ -50,9 +50,12 @@ class particle {
     double light = 2.9979e8 * Unit::m / Unit::sec;                    //the speed of light in AU/s
     //double k_n = 1.*pow(10,12.)/(AU*AU);                        //normalization diffusion coefficient in AU^2/s corresponging to 10^22 cm^2/s
     double boundary = 110 * Unit::AU;                                     //boundary condition in AU
-    double dt = 2.0 * Unit::sec;                                             //time interval per step
+    double dt = 0.1 * Unit::sec;                                             //time interval per step
     double Vs;                                                  //solar wind velocity
+    double Vs_eq;
     double Omega = 2*Unit::pi/27.5/Unit::day;                        //angular velocity corresponding to 27.5 day
+    double t0 = 0.0;
+    double t = 0.0;
 
     double polarity;                                            //field direction
     double angle;                                               //tilt angle of HCS
@@ -69,7 +72,8 @@ class particle {
 
     double Br;                                                  //magnetic field components in three direction
     double Bp;                                                  //
-    double Bt = 0. ;                                            //
+    double Bn;
+    double Bt = 0.0;                                            //
     double psi;                                                 //pitch angle between field and radial direction
     double theta_s;                                             //tile angle of HCS at particle point
     double heaviside;                                           //field direction in particle point
@@ -79,14 +83,12 @@ class particle {
     double Z = 1.;                                              //charge number, proton by default
     double mass;                                                  //rest mass
 
-
     double Vdr_gc;                                              //drift to radial direction
     double Vdp_gc;                                              //drift to azimuthal direction
     double Vdt_gc;                                              //drift to pole direction
     double Vdr_HCS = 0;                                             //drift at the HCS for thress direction
     double Vdp_HCS = 0;
     double Vdt_HCS = 0;
-
 
 
     public:
@@ -97,7 +99,7 @@ class particle {
     void step();                                                //simulate trajectory of particle
 
     const double Wind();                                        //solar wind velocity function
-    const double Theta_S();                                     //theat_s function
+    double Theta_S(double, double);                                     //theat_s function
     const double Heav();                                        //get heaviside function
     const double B_r(const double &heaviside);                  //radial magnetic field function
     const double B_p(const double &heaviside);                  //azimuthal magnetic field function
@@ -107,17 +109,15 @@ class particle {
     const double K_tt();
     const double K_pp();
 
-
+    void HCS_rphi(const double &r, const double &phi, double& x, double& y, double& z) const;
+    //double HCS_xy_z(const double &x, const double &y) const;
 
     double Ek;                                                  //kinetic energy
     double M_p;                                                 // momentum of particle
     double V_p;                                                 // velocity of praticle
-    double r = (1. + pow(10.,-10.))*Unit::AU;                              // radial distance
-    double theta = Unit::deg*90.+pow(10.,-10.);
+    double r = (/*3.4147*/ 1. + pow(10.,-10.))*Unit::AU;                              // radial distance
+    double theta = Unit::deg*(90)+pow(10.,-10.);
     double phi = pow(10.,-10.);
-
-
-
 };
 
 

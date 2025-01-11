a1c92ca9ab3a0363a891d80ccce3c306e7c4577c
zhm <zhanghm36@mail2.sysu.edu.cn>
2025-01-11 16:05:17 +0800

    update fcache to be capable of caching func in customize x_range

--- src/fcache.h
+++ src/fcache.h
@@ -5,17 +5,21 @@
 #include <cmath>
 #include <vector>
 
+const double pi_2 = std::acos(-1) * 2;
+
 class fcache {
   public:
-    fcache(const std::function<double(double)>& f, int npix_);
+    fcache(const std::function<double(double)>& f, int npix_, double x_min = 0, double x_max = pi_2);
     ~fcache();
 
     double operator()(double x) const;
 
-  private:
-    const double pi_2 = std::acos(-1) * 2;
+  public:
+    // const double pi_2 = std::acos(-1) * 2;
+    double x_min, x_max, x_range;
+    bool triangularQ = false;
     int npix;
-    double dphi;
+    double dx;
     std::vector<double> vals;
 };
 
